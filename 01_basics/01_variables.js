f7d44f4c370e06ab2ee2e3499e1b6f41b63c01f2
Prince Kumar Barnwal <131050575+princekbarnwal@users.noreply.github.com>
2025-06-15 05:48:47 +0000

    DONE

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId=11621
+let accountEmail="xyz@gmail.com"
+var accountPassword="xyzabc"
+accountCity="Kolkata"
+let accountState
+
+/* let and var use forall other 
+    but avoid var use only let
+    because of issue in block scope and functional scope
+*/
+accountEmail="abc@xyz.com"
+accountPassword="abc123456"
+accountCity="Delhi"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all code as a newer version
+
+// alert(4 + 3) it will not run because we are using nodeJS not browser
+
+// code readability should be high
+
+let name = "prince"
+let age = 22
+let isLoggedIn = true
+let state 
+
+// number range 2 to the power 53
+// bigInt 
+// String => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(name)
+console.log(isLoggedIn)
+console.log(age)
+console.log(typeof age)
