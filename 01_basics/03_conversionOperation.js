4cd1be0140ffc37962e7e871cddd970cd11b2d65
Prince Kumar Barnwal <131050575+princekbarnwal@users.noreply.github.com>
2025-06-21 08:04:45 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33abc"
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNumber=Number(score);
+console.log(valueInNumber);
+console.log(typeof(valueInNumber));
+
+console.log("\n");
+
+// 33 will easily convert into number
+// typeof will convert 33abc into number th type of will print number
+// but it will not print the no. it will print Nan i.e. not a number
+
+let isLoggedIn="prince";
+let valueInisLoggedIn=Boolean(isLoggedIn);
+console.log(valueInisLoggedIn);
+
+console.log("\n");
+// "" empty will convert into false "filled" filled string will give true
+// 1 = true , 0 = false
+
+let int=5;
+console.log(int);
+console.log(typeof(int));
+let str=String(int);
+console.log(str);
+console.log(typeof(str));
+
+
+
