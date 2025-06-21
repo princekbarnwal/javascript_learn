1d1f16303a4f3f192a80596bd2d518d598dd39ff
Prince Kumar Barnwal <131050575+princekbarnwal@users.noreply.github.com>
2025-06-21 09:18:00 +0000

    done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33abc"
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 let valueInNumber=Number(score);
-console.log(valueInNumber);
-console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+// console.log(typeof(valueInNumber));
 
-console.log("\n");
+// console.log("\n");
 
 // 33 will easily convert into number
 // typeof will convert 33abc into number th type of will print number
@@ -13,18 +13,57 @@ console.log("\n");
 
 let isLoggedIn="prince";
 let valueInisLoggedIn=Boolean(isLoggedIn);
-console.log(valueInisLoggedIn);
+// console.log(valueInisLoggedIn);
 
-console.log("\n");
+// console.log("\n");
 // "" empty will convert into false "filled" filled string will give true
 // 1 = true , 0 = false
 
 let int=5;
-console.log(int);
-console.log(typeof(int));
+// console.log(int);
+// console.log(typeof(int));
 let str=String(int);
-console.log(str);
-console.log(typeof(str));
+// console.log(str);
+// console.log(typeof(str));
 
+// ***************************Operations*****************************
 
+let num=3;
+let neg=-num;
+// console.log(neg);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**3);
 
+let str1 = "hlo";
+let str2 = " prince";
+// console.log(str1 + str2);
+
+// console.log("1" + 2); 
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// if string is first all will convert to string
+// if string is last first operation and then all will convert to string
+
+// console.log(+true);  // print 1
+// console.log(+false); // print 0
+// console.log(-true); // print -1
+// console.log(-false); // print -0
+// console.log(+"0"); // print 0
+// console.log(+""); // print 0
+// console.log(+"8"); // print the number
+// console.log(+"hlo"); // print NaN
+// console.log(-""); // print -0
+
+let num1 , num2 , num3
+// num1 = num2 = num3 = 2 + 2
+// console.table([num1 , num2 , num3]);
+
+let gameCounter = 100;
+// console.log(gameCounter++);
+// console.log(gameCounter);
