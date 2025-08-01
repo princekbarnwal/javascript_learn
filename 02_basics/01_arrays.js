7ae6cb6c8b22106eb35bc214d820fae70b7d6e09
Prince Kumar Barnwal <131050575+princekbarnwal@users.noreply.github.com>
2025-08-01 02:16:28 +0000

    02_basics add

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,14 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+console.log(myArr[0]);
+
+const Heroes = ["Shaktimaan", "Thor", "Ironman", "Superman"]
+const myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr2.Array());
+
+
+
+
+
+
